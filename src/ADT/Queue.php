d48021bc39f81eb60ae570c620d564fe7489002c
Martins Luters <luters.martins@gmail.com>
2023-02-14 13:29:16 +0200

    Implement Queue

--- src/ADT/Queue.php
+++ src/ADT/Queue.php
@@ -4,5 +4,26 @@ namespace Braddle\PhpUk2023\ADT;
 
 class Queue
 {
+	private int $count = 0;
+	private array $queue = [];
+	private int $nextElementIndex = 0;
+	public function isEmpty(): bool {
+		return ! $this->count;
+	}
+
+	public function push( string $value ) : void {
+		$this->queue[] = $value;
+		$this->count++;
+	}
+
+	public function count() : int {
+		return $this->count;
+	}
+
+	public function pop(): string {
+		$this->count--;
+		return $this->queue[ $this->nextElementIndex++ ];
+	}
+
 
 }
--- test/ADT/QueueTest.php
+++ test/ADT/QueueTest.php
@@ -1,13 +1,86 @@
 <?php
 
-namespace ADT;
+namespace Braddle\PhpUk2023\ADT;
 
 use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    public function testHolding()
-    {
-        $this->assertTrue(true);
-    }
+	public Queue $sut;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->sut = new Queue();
+	}
+
+	public function testIsEmpty()
+	{
+		$this->assertTrue( $this->sut->isEmpty() );
+	}
+
+	public function testIsNotEmpty()
+	{
+		$this->sut->push( 'one' );
+		$this->assertFalse( $this->sut->isEmpty() );
+	}
+
+	public function testContainsOneElement()
+	{
+		$this->sut->push( 'one' );
+		$this->assertSame( 1, $this->sut->count() );
+	}
+
+	public function testContainsTwoElements()
+	{
+		$this->sut->push( 'one' );
+		$this->sut->push( 'two' );
+		$this->assertSame( 2, $this->sut->count() );
+	}
+
+	public function testPopReturnsTheOnlyElement()
+	{
+		$this->sut->push( 'one' );
+		$this->assertSame( 'one', $this->sut->pop() );
+	}
+
+	public function testReturnsTheLastInsertedElement()
+	{
+		$this->sut->push( 'one' );
+		$this->sut->push( 'two' );
+		$this->assertSame( 'one', $this->sut->pop() );
+		$this->assertSame( 'two', $this->sut->pop() );
+	}
+
+	public function testReturnsTheLastInsertedElementInSequence()
+	{
+		$this->sut->push( 'one' );
+		$this->sut->push( 'two' );
+		$this->sut->pop();
+		$this->sut->pop();
+
+		$this->sut->push( 'three' );
+		$this->sut->push( 'four' );
+		$this->assertSame( 'three', $this->sut->pop() );
+		$this->assertSame( 'four', $this->sut->pop() );
+	}
+
+	public function testCountRemainsConsistent() {
+		$this->sut->push( 'one' );
+		$this->sut->push( 'two' );
+		$this->sut->pop();
+		$this->sut->pop();
+		$this->assertSame( 0, $this->sut->count() );
+	}
+
+	public function testCountRemainsConsistent2() {
+		$this->sut->push( 'one' );
+		$this->sut->push( 'two' );
+		$this->sut->pop();
+		$this->sut->pop();
+		$lastElement = $this->sut->push( 'three' );
+		$this->assertSame( 1, $this->sut->count() );
+		$this->assertSame( 'three', $this->sut->pop() );
+	}
 }
